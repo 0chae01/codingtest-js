a8220b70c8d683dd5280d8b7e9c1b550409e15a3
youngchae <010101chae@gmail.com>
2025-07-07 08:58:57 +0900

    택배 박스 정리 시스템(스택)

---
+++ study/problems/1.js
@@ -0,0 +1,16 @@
+// 택배 박스 정리 시스템 (스택)
+
+function solution(boxes) {
+    const stack = [];
+
+    for (const box of boxes) {
+        if (stack[stack.length - 1] !== box) {
+            stack.push(box);
+        }
+    }
+
+    return stack;
+}
+
+console.log(solution([2, 2, 5, 5, 5, 1, 1]));
+console.log(solution([7, 7, 8, 8, 0, 0, 0]));
