cb154d394b9559f7df49bdfc50d9c096cec14d36
youngchae <010101chae@gmail.com>
2025-07-15 08:18:25 +0900

    bubble sort

---
+++ study/bubble-sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(array) {
+  const n = array.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (array[j] > array[j + 1]) {
+        // let temp = array[j];
+        // array[j] = array[j + 1];
+        // array[j + 1] = temp;
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([77, 42, 6, 4, 1, 2, 5, 6, 2]));
